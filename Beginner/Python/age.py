5e27dedfd9800756cca9a72e3f9480bd3917267f
Titan <sahilahuja1911@gmail.com>
2020-10-03 17:10:53 +0530

    .

---
+++ Beginner/Python/age.py
@@ -0,0 +1,15 @@
+# github username : Titan1911
+
+# program to check the whether the person is an adult, teen or a child
+
+age = int(input('Enter your age : '))
+
+if age>18:
+    print('You are an adult!')
+
+if age<=18 and age>10:
+    print('You are a teen!')
+
+if age<=10:
+    print('You are a child!')
+
