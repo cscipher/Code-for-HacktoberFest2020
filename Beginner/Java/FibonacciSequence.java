ab5f6ff52ff3317c634f01643260c9ed0503f167
Tran Minh Quy <minhquylt95@gmail.com>
2020-10-17 15:50:24 +0700

    Create FibonacciSequence.java
    Solution for Fibonacci Sequence

---
+++ Beginner/Java/FibonacciSequence.java
@@ -0,0 +1,17 @@
+#github username: quytm
+
+class FibonacciSequence {
+
+    private static int[] fibonacciSequence2(int n) {
+        int[] result = new int[n + 1];
+        result[1] = 0;
+        result[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i-1] + result[i-2];
+        }
+
+        return result;
+    }
+
+}
